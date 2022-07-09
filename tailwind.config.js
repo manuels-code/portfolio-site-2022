bf2337baaf5955e02263a114b581a66159c281bb
manuels-code <101575315+manuels-code@users.noreply.github.com>
2022-07-09 11:25:27 -0500

    Update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
         lg: ["20px", "28px"],
         xl: ["24px", "32px"],
       },
+      backgroundImage: {
+        "hero-image":
+          "url('https://preview.colorlib.com/theme/knuford/images/xbg_1.jpg.pagespeed.ic.LOd_TK0d0a.webp')",
+      },
       colors: {
         "app-base-1": "#0779e4",
         "app-base-2": "#57abfa",
@@ -15,6 +19,7 @@ module.exports = {
         "app-text-1": "#000000cc",
         "app-text-2": "#3b3b3b",
         "app-text-2": "#a6a6a6",
+        "app-bg-1": "#f8f8f8",
       },
     },
   },
