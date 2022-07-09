c0091851aae41b509ed372f9075e0457896e9114
manuels-code <101575315+manuels-code@users.noreply.github.com>
2022-07-09 11:24:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Header from './components/layout/Header'
+import React from "react";
+import Hero from "./components/Hero";
+import Header from "./components/layout/Header";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
