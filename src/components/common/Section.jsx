c8b18f4c5e382d269582d8707e0d979abe2780be
manuels-code <101575315+manuels-code@users.noreply.github.com>
2022-07-09 11:24:26 -0500

    Update Section

--- src/components/common/Section.jsx
+++ src/components/common/Section.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Container from "./Container";
 
-const Section = ({ children }) => {
+const Section = ({ children, bg }) => {
+  const bgList = [`bg-app-bg-1`, `bg-hero-image`];
+
+  const checkBgs = bgList.includes(bg) ? bg : bgList[0];
+
   return (
-    <section>
+    <section className={`${checkBgs} bg-cover`}>
       <Container>{children}</Container>
     </section>
   );
